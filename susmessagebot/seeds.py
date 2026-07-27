3c6c897d0a52089f89c53001677946e3fbbef2c8
TheresaQAQ <63690853+TheresaQAQ@users.noreply.github.com>
2026-07-27 10:20:59 +0800

    Add SAFE example via HITL feedback

--- susmessagebot/seeds.py
+++ susmessagebot/seeds.py
@@ -62,6 +62,7 @@ examples = [
     ('Earn $5000 a week working from home! DM me now for details', "BAN"),
     ('Check this out: http://malware.wicar.org', "BAN"),
     ('Их шмотки по штуке за компанию обычную и за компанию героик', "SAFE"),
+    ('У него 40 к драконки', "SAFE"),
 ]
 
 
